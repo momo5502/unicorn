fcdceccc639446a8d2634f74e1cea439355a2a3d
momo5502 <mauriceheumann@gmail.com>
2025-04-18 16:41:02 +0200

    Wrap inline hook arguments

--- qemu/tcg/tcg.c
+++ qemu/tcg/tcg.c
@@ -664,6 +664,21 @@ static void process_op_defs(TCGContext *s);
 static TCGTemp *tcg_global_reg_new_internal(TCGContext *s, TCGType type,
                                             TCGReg reg, const char *name);
 
+static void forward_inline_hook(uc_engine *uc, uint64_t address, uint32_t size, void *user_data) {
+    struct hook* hk = (struct hook *)user_data;
+   ((uc_cb_hookcode_t)hk->callback)(uc, address, size, hk->user_data);
+}
+
+static uint64_t run_inline_hook(size_t a1, size_t a2, size_t a3, size_t a4, size_t a5, size_t a6, size_t a7, size_t a8, size_t a9, size_t a10, size_t a11, size_t a12) {
+    if (sizeof(a1) == sizeof(uint64_t)) {
+        forward_inline_hook((uc_engine*)a1, a2, (uint32_t)a3, (void*)a4);
+        return 0;
+    }
+
+    forward_inline_hook((uc_engine*)a1, (a2 | ((uint64_t)a3 << 32)), a4, (void*)a5);
+    return 0;
+}
+
 void uc_add_inline_hook(uc_engine *uc, struct hook *hk, void** args, int args_len)
 {
     TCGHelperInfo* info = g_malloc(sizeof(TCGHelperInfo));
@@ -672,7 +687,7 @@ void uc_add_inline_hook(uc_engine *uc, struct hook *hk, void** args, int args_le
     TCGContext *tcg_ctx = uc->tcg_ctx;
     GHashTable *helper_table = uc->tcg_ctx->helper_table;
 
-    info->func = hk->callback;
+    info->func = &run_inline_hook;
     info->name = name;
     info->flags = 0; // From helper-head.h
 
